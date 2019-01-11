03c043affeea75760dfd25170660e7f35f144134
Olessia Potapova <olessia.potapova@gmail.com>
2019-01-11 13:48:48 -0300

    added newBook button

---
+++ src/NewBook.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const NewBook = () => (
+    <div className="new-book">
+        <i class="fas fa-plus-circle"></i>
+        <div>
+            <button className="addButton">Agregar libro</button>
+        </div>
+    </div>
+)
+
+export default NewBook;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NewBook from './NewBook'
 import BookItems from './BookItems';
 import Header from './Header';
 import SearchBar from './SearchBar';
@@ -8,36 +9,14 @@ import listaLibros from './listaLibros';
 import './style.css';
 
 const BookIndex = () => (
-    <div>
+    <div className="app">
         <Header />
         <SearchBar />
         <SubHeader />
         <BookItems books={listaLibros}/>
+        <NewBook />
     </div>
 )
 
-// const testBooks = [
-//     {
-//         id:1,
-//         name:'book 1',
-//         editor: 'editor 1',
-//         author: 'author 1',
-//         edition: 1200,
-//     },
-//     {
-//         id:2,
-//         name:'book 2',
-//         editor: 'editor 2',
-//         author: 'author 2',
-//         edition: 1200,
-//     },
-//     {
-//         id:3,
-//         name:'book 3',
-//         editor: 'editor 3',
-//         author: 'author 3',
-//         edition: 1200,
-//     }
-//]
 
 ReactDOM.render(<BookIndex books={listaLibros}/>, document.querySelector('#root'));
