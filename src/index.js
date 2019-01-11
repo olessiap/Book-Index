c7b2031dce1ce24f2b1cefc39b94acdcb6369231
Olessia Potapova <olessia.potapova@gmail.com>
2019-01-11 10:45:56 -0300

    books are being displayed from listaLibros json now

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import BookItems from './BookItems';
 import Header from './Header';
 import SearchBar from './SearchBar';
 import SubHeader from './SubHeader';
+import listaLibros from './listaLibros';
 import './style.css';
 
 const BookIndex = () => (
@@ -11,32 +12,32 @@ const BookIndex = () => (
         <Header />
         <SearchBar />
         <SubHeader />
-        <BookItems books={testBooks}/>
+        <BookItems books={listaLibros}/>
     </div>
 )
 
-const testBooks = [
-    {
-        id:1,
-        name:'book 1',
-        editor: 'editor 1',
-        author: 'author 1',
-        edition: 1200,
-    },
-    {
-        id:2,
-        name:'book 2',
-        editor: 'editor 2',
-        author: 'author 2',
-        edition: 1200,
-    },
-    {
-        id:3,
-        name:'book 3',
-        editor: 'editor 3',
-        author: 'author 3',
-        edition: 1200,
-    }
-]
+// const testBooks = [
+//     {
+//         id:1,
+//         name:'book 1',
+//         editor: 'editor 1',
+//         author: 'author 1',
+//         edition: 1200,
+//     },
+//     {
+//         id:2,
+//         name:'book 2',
+//         editor: 'editor 2',
+//         author: 'author 2',
+//         edition: 1200,
+//     },
+//     {
+//         id:3,
+//         name:'book 3',
+//         editor: 'editor 3',
+//         author: 'author 3',
+//         edition: 1200,
+//     }
+//]
 
-ReactDOM.render(<BookIndex books={testBooks}/>, document.querySelector('#root'));
+ReactDOM.render(<BookIndex books={listaLibros}/>, document.querySelector('#root'));
